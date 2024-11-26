be9b8b17e55bad101076b8a80502a83d10d4cd13
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-26 14:10:36 -0500

    Alumno Controller

--- usuarios/src/main/java/com/rangel/usuarios/controller/AlumnoController.java
+++ usuarios/src/main/java/com/rangel/usuarios/controller/AlumnoController.java
@@ -1,9 +1,62 @@
 package com.rangel.usuarios.controller;
 
+import com.rangel.usuarios.entity.Alumno;
 import com.rangel.usuarios.service.AlumnoServiceImpl;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-public class AlumnoController {
+import java.util.Optional;
+
+@RestController
+public class AlumnoController{
     @Autowired
     AlumnoServiceImpl service;
+
+    @GetMapping
+    public ResponseEntity<?> listarAlumno(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/{id")
+    public ResponseEntity<?> ver(@PathVariable Long id){
+        Optional<Alumno> ob = service.findById(id);
+
+        if (ob.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok().body(ob.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crear (@RequestBody Alumno alumno){
+        Alumno alumnoDb = service.save(alumno);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar (@RequestBody Alumno alumno, @PathVariable Long id){
+        Optional<Alumno> ob = service.findById(id);
+
+        if (ob.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        Alumno alumnoDb = ob.get();
+        alumnoDb.setNombre(alumno.getNombre());
+        alumnoDb.setApellido(alumno.getApellido());
+        alumnoDb.setEmail(alumno.getEmail());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<?> eliminar (@PathVariable Long id){
+        service.deletedById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
