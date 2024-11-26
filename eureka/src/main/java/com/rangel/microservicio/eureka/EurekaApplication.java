4e9ce4295c918ef2b0e51d23639d0ac5d0bf2027
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-26 14:25:00 -0500

    MicroServicio Eureka

---
+++ eureka/src/main/java/com/rangel/microservicio/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.rangel.microservicio.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
---
+++ eureka/src/test/java/com/rangel/microservicio/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangel.microservicio.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
