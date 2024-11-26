3beaa866134899a478edb276951d9fa46e3c951c
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-26 14:28:50 -0500

    Cofiguracion Eureca S

--- eureka/src/main/java/com/rangel/microservicio/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/rangel/microservicio/eureka/EurekaApplication.java
@@ -2,7 +2,9 @@ package com.rangel.microservicio.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
