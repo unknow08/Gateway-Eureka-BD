636b8681bba880595ce0775ccdae48e06c4c3791
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 16:46:38 -0500

    CursoService

---
+++ curso/src/main/java/com/rangel/curso/service/CursoService.java
@@ -0,0 +1,7 @@
+package com.rangel.curso.service;
+
+import com.rangel.commons.service.CommonService;
+import com.rangel.curso.models.entity.Curso;
+
+public interface CursoService extends CommonService<Curso> {
+}
