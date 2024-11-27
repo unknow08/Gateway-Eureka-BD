05f1232977042f69d07f2eda52428d9633c0ed54
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 17:57:18 -0500

    Metodos put para agregar y eliminar alumno en curso

--- curso/src/main/java/com/rangel/curso/controller/CursoController.java
+++ curso/src/main/java/com/rangel/curso/controller/CursoController.java
@@ -1,5 +1,6 @@
 package com.rangel.curso.controller;
 
+import com.rangel.common.CommonUsuario.models.entity.Alumno;
 import com.rangel.commons.controller.CommonController;
 import com.rangel.curso.models.entity.Curso;
 import com.rangel.curso.service.CursoService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,5 +29,34 @@ public class CursoController extends CommonController<Curso, CursoService> {
         cursoDb.setNombre(curso.getNombre());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(cursoDb));
+    }
+
+    @PutMapping("/{id}/asignar-alumno")
+    public ResponseEntity<?> asignarAlumno (@RequestBody List<Alumno> alumno, @PathVariable Long id){
+        Optional<Curso> ob = service.findById(id);
+
+        if(ob.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        Curso cursoBd = ob.get();
+        alumno.forEach(a ->{
+            cursoBd.setAlumnos(a);
+        });
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(cursoBd));
+    }
+
+    @PutMapping("/{id}/eliminar-alumno")
+    public ResponseEntity<?> eliminarAlumno (@RequestBody Alumno alumno, @PathVariable Long id){
+        Optional<Curso> ob = service.findById(id);
+
+        if(ob.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        Curso cursoBd = ob.get();
+        cursoBd.removeAlumnos(alumno);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(cursoBd));
     }
 }
