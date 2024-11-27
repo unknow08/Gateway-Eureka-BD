0d92f23802665fec8d1b4bff72cefab361d3b647
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 16:51:00 -0500

    Anotacion service en CursoServiceImpl

--- curso/src/main/java/com/rangel/curso/service/CursoServiceImpl.java
+++ curso/src/main/java/com/rangel/curso/service/CursoServiceImpl.java
@@ -4,6 +4,8 @@ import com.rangel.commons.service.CommonService;
 import com.rangel.commons.service.CommonServiceImpl;
 import com.rangel.curso.models.entity.Curso;
 import com.rangel.curso.repository.CursoRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CommonService<Curso> {
 }
