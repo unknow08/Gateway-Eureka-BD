1f0ea80f508ab20a1de53c6d4798634156d6e779
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 14:35:56 -0500

    Libreria commons

---
+++ commons/src/test/java/com/rangel/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangel.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
