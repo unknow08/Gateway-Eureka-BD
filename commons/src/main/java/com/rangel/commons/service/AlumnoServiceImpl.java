a3480a3bcd6369a5fa51312844010c296d4d2a24
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 14:41:28 -0500

    copia de service e impl de usuarios a commons

---
+++ commons/src/main/java/com/rangel/commons/service/AlumnoServiceImpl.java
@@ -0,0 +1,39 @@
+package com.rangel.commons.service;
+
+import com.rangel.usuarios.entity.Alumno;
+import com.rangel.usuarios.repository.AlumnoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class AlumnoServiceImpl implements AlumnoService{
+    @Autowired
+    AlumnoRepository dao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Iterable<Alumno> findAll() {
+        return dao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Alumno> findById(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Alumno save(Alumno alumno) {
+        return dao.save(alumno);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public void deletedById(Long id) {
+        dao.deleteById(id);
+    }
+}
