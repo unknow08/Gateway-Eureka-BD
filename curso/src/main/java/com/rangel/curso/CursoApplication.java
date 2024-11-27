961798628c550a659929382e8acffafa07203727
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 17:23:59 -0500

    Dependencia y componentScan de commonUsuario a Curso

--- curso/src/main/java/com/rangel/curso/CursoApplication.java
+++ curso/src/main/java/com/rangel/curso/CursoApplication.java
@@ -3,9 +3,11 @@ package com.rangel.curso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan({"com.rangel.common.CommonUsuario.models.entity"})
 public class CursoApplication {
 
 	public static void main(String[] args) {
