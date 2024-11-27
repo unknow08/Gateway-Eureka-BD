f42823d6f48d7370ba6b0d14f4c0f9e59e386d78
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-11-27 16:50:14 -0500

    CursoServiceImpl

---
+++ curso/src/main/java/com/rangel/curso/service/CursoServiceImpl.java
@@ -0,0 +1,9 @@
+package com.rangel.curso.service;
+
+import com.rangel.commons.service.CommonService;
+import com.rangel.commons.service.CommonServiceImpl;
+import com.rangel.curso.models.entity.Curso;
+import com.rangel.curso.repository.CursoRepository;
+
+public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CommonService<Curso> {
+}
